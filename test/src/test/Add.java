2b044a6dbbc848ee68a609f471d96277782dd307
HP <HP@LAPTOP-MBU8NU9S>
2022-02-08 11:37:15 +0530

    Addition

---
+++ test/src/test/Add.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int num1=4, num2=6, sum;
+		sum= num1+num2;
+		
+		System.out.println("sum of these num :"+sum);
+
+	}
+
+}
